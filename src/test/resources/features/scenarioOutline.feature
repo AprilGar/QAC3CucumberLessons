7ef6242f203ddf5300243568bc2a7cca39e805fc
AprilGar <april.garlington@hotmail.co.uk>
2025-06-18 11:53:38 +0100

    code along with Anjali

--- src/test/resources/features/scenarioOutline.feature
+++ src/test/resources/features/scenarioOutline.feature
@@ -1,10 +1,11 @@
-Feature: Background Feature
+Feature: Scenario Outline Feature
 
   Background:
     Given the user is on the login page
 
 #    Scenario outline will run through each row of the examples table
   Scenario Outline: Login with invalid credentials
+#    The values that will be substituted for the values in the examples table should be wrapped in "<>"
     When the user enters "<username>" and "<password>"
     And the user clicks the login button
     Then the user should be logged in successfully
@@ -14,3 +15,5 @@ Feature: Background Feature
     | april | 123456 |
     | chay | 345@345 |
     | mercator | 76543 |
+    | mercator | |
+#    empty value will be accepted as a null value exception
--- src/test/scala/stepdefs/Login1.scala
+++ src/test/scala/stepdefs/Login1.scala
@@ -1,22 +1,34 @@
 package stepdefs
 
 import io.cucumber.scala.{EN, PendingException, ScalaDsl}
+import org.openqa.selenium.{By, WebDriver, WebElement}
+import org.openqa.selenium.chrome.ChromeDriver
 
 class Login1 extends ScalaDsl with EN {
 
+  val driver: WebDriver = new ChromeDriver() //keep at top level so everything has access.
+
   //Each of these is a step definition
-  Given ("""the user is on the login page"""){ () =>
+  Given("""the user is on the login page""") { () =>
     // Write code here that turns the phrase above into concrete actions
+    driver.get("https://parabank.parasoft.com/parabank/index.html")
   }
-    When ("""the user enters valid user name and password""") { () =>
-
-    }
-  And ("""the user clicks the login button""") {() =>
-
+  When("""the user enters valid user name and password""") { () =>
+    val userName: WebElement = driver.findElement(By.name("username")) //cannot send keys on this line due to it converting the type to `unit`
+    userName.sendKeys("john")
+    val password: WebElement = driver.findElement(By.name("password"))
+    password.sendKeys("demo")
   }
-  Then ("""the user should be logged in successfully""") {() =>
-
+  And("""the user clicks the login button""") { () =>
+    val submitButton = driver.findElement(By.cssSelector("input[type='submit']"))
+    submitButton.click()
+    Thread.sleep(2000)
   }
-
+  Then("""the user should be logged in successfully""") { () =>
+val checkPageTitle = driver.findElement(By.tagName("h1")).getText
+    println("Page Title: " + checkPageTitle) //Just using to visualise the text returned prior to method.
+    if (checkPageTitle == "Accounts Overview") println("Logged in!")
+    else println("Issue...")
+  }
 }
 
