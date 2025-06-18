3022ddebeadd7331ec9426f07370254e93e301b5
AprilGar <april.garlington@hotmail.co.uk>
2025-06-18 10:46:31 +0100

    add feature files

--- src/test/scala/stepdefs/Login1.scala
+++ src/test/scala/stepdefs/Login1.scala
@@ -3,9 +3,20 @@ package stepdefs
 import io.cucumber.scala.{EN, PendingException, ScalaDsl}
 
 class Login1 extends ScalaDsl with EN {
-  Given("""the user is on the login page""") { () =>
-    // Write code here that turns the phrase above into concrete actions
 
-     throw new PendingException()  }
+  //Each of these is a step definition
+  Given ("""the user is on the login page"""){ () =>
+    // Write code here that turns the phrase above into concrete actions
+  }
+    When ("""the user enters valid user name and password""") { () =>
+
+    }
+  And ("""the user clicks the login button""") {() =>
+
+  }
+  Then ("""the user should be logged in successfully""") {() =>
+
+  }
+
 }
 
